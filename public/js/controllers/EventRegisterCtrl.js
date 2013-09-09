ced3bee49be20d2a55f3e2ad4e58eee7f29d901c
Gonzalo Beviglia <gonzalo.beviglia@fdvsolutions.com>
2013-09-09 18:13:47 -0300

    Lista de miembros

--- public/js/controllers/EventRegisterCtrl.js
+++ public/js/controllers/EventRegisterCtrl.js
@@ -9,4 +9,28 @@ angular.module(controllerName, []).
 
         $scope.$emit("BackgroundChange", "event-register-background");
 
+        $scope.teamMembers = [];
+
+        $scope.newMember = '';
+
+        var triggerDigest = function () {
+            if (!$scope.$$phase) {
+                $scope.$digest();
+            }
+
+        }
+
+        $scope.addMember = function () {
+            if ($scope.teamMembers.length < 5) {
+                $scope.teamMembers.push($scope.newMember);
+                $scope.newMember = '';
+                triggerDigest();
+            }
+        }
+
+        $scope.removeMember = function (index) {
+            $scope.teamMembers.splice(index, 1);
+            triggerDigest();
+        }
+
     }]);
